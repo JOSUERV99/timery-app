ba5ec635873050c964da10489ff8449cae15784b
JOSUERV99 <josuerojasvega@gmail.com>
2022-04-19 07:25:45 -0600

    Create and get timer from local db

--- backend/src/routes/timer.js
+++ backend/src/routes/timer.js
@@ -6,16 +6,28 @@ const router = express.Router();
 const MAIN_TABLE = process.env.MYSQL_MAIN_TABLE;
 
 router.get('/', async (req, res) => {
-  const result = await query(`SELECT id, creationDate, rest, work, sets, totalTime FROM ${MAIN_TABLE}`);
   try {
+    const result = await query(`SELECT id, creationDate, rest, work, sets, totalTime FROM ${MAIN_TABLE}`);
     res.json(result);
   } catch (err) {
     console.error(err.message);
   }
 });
 
-router.post('/create', (req, res) => {
-    
+router.post('/create', async (req, res) => {
+  try {
+    const element = req.body.timer;
+    const result = await query(
+      `INSERT INTO ${MAIN_TABLE} (rest, sets, work, creationDate, totalTime, name) VALUES (?, ?, ?, ?, ?, ?)`, 
+      [element.rest, element.sets, element.work, new Date(), element.totalTime, element.name]
+    );
+
+    if (result.affectedRows) {
+      res.json({...result});
+    }
+  } catch (err) {
+    res.json({error:err.message, affectedRows : 0});
+  }
 });
 
 router.put('/modify', (req, res) => {
